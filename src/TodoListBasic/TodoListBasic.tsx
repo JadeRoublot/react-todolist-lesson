db9a997bc7f3deb291efa9d06e72b6638e9852ca
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2023-01-19 15:23:44 +0100

    Update TodoListBasic.tsx

--- src/TodoListBasic/TodoListBasic.tsx
+++ src/TodoListBasic/TodoListBasic.tsx
@@ -4,10 +4,34 @@ import React, { useState } from 'react';
 const TodoListBasic = () => {
 
     const [item, setItem ] = useState();
+    const [state, setState ] = useState();
+
+    const handleAdd = () =>
+    console.log("hello");
+
+    const handleChange = (event:any) =>
+    setState(event.target.value);
 
 
+    return (<div>
+        
+            <input type="text" name="item" />
 
-    return <div>TodoListBasic</div>;
+            <label>
+                <select value={state} onChange={handleChange}>
+                    <option value="To do">To do</option>
+                    <option value="In progress">In progress</option>
+                    <option value="Done">Done</option>
+                </select>
+            </label>
+
+            <button type="button" onClick={handleAdd}>
+                Add to list
+            </button>
+        
+        </div>
+    
+    );
 };
 
 export default TodoListBasic;
